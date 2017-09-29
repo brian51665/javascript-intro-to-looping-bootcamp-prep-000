501c1f67cc776ec7952c0b2744150fc7f457e6a2
brian51665 <bdavis51892@yahoo.com>
2017-09-29 02:00:55 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+
+function forLoop(array){
+  for (let i = 0; i < 26; i++) {
+    if (i === 1)
+      array.push(`"I am ${i} strange loop."`)
+    else if  (i > 1)
+      array.push(`"I am ${i} strange loops."`)
+}
+return array
+}
+
+function whileLoop(n) {
+  while (> 0) {
+    console.log(n)
+  }
+  return 'done'
+}
